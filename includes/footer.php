1f9f2f5ebd707d458da23809cd01934b6191a6ef
nyecruz <130216600+nyecruz@users.noreply.github.com>
2023-04-08 17:25:18 +0800

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,92 @@
+<footer id="footer" class="footer color-bg">
+	  <div class="links-social inner-top-sm">
+        <div class="container">
+            <div class="row">
+            	<div class="col-xs-12 col-sm-6 col-md-4">
+            		 <!--  CONTACT INFO -->
+<div class="contact-info">
+    <div class="footer-logo">
+        <div class="logo">
+            <a href="index.php">
+                
+<h3>Follow Us</h3>
+            </a>
+        </div>
+    
+    </div>
+
+     <div class="module-body m-t-20">
+        <p> </p>
+    
+        <div class="social-icons">
+            
+        <a href="http://facebook.com/nyex02" class='active'><i class="icon fa fa-facebook"></i></a>
+        <a href="#"><i class="icon fa fa-twitter"></i></a>
+        <a href="#"><i class="icon fa fa-linkedin"></i></a>
+        <a href="#"><i class="icon fa fa-rss"></i></a>
+        <a href="#"><i class="icon fa fa-pinterest"></i></a>
+
+        </div>
+    </div>
+
+</div>
+</div>
+                        <!--  CONTACT INFO : END  -->         
+
+            	<div class="col-xs-12 col-sm-6 col-md-4">
+                </div>
+                                <!-- INFORMATION-->
+<div class="col-xs-12 col-sm-6 col-md-4">
+<div class="contact-information">
+	<div class="module-heading">
+		<h4 class="module-title">information</h4>
+	</div>
+
+	<div class="module-body outer-top-xs">
+        <ul class="toggle-footer" style="">
+            <li class="media">
+                <div class="pull-left">
+                     <span class="icon fa-stack fa-lg">
+                      <i class="fa fa-circle fa-stack-2x"></i>
+                      <i class="fa fa-map-marker fa-stack-1x fa-inverse"></i>
+                    </span>
+                </div>
+                <div class="media-body">
+                <a href="https://goo.gl/maps/aXHfrGEDYJiyB4eD8">Talavera, Nueva Ecija, Philippines</a>
+                </div>
+            </li>
+
+              <li class="media">
+                <div class="pull-left">
+                     <span class="icon fa-stack fa-lg">
+                      <i class="fa fa-circle fa-stack-2x"></i>
+                      <i class="fa fa-mobile fa-stack-1x fa-inverse"></i>
+                    </span>
+                </div>
+                <div class="media-body">
+                    <p>09369270105</p>
+                </div>
+            </li>
+
+              <li class="media">
+                <div class="pull-left">
+                     <span class="icon fa-stack fa-lg">
+                      <i class="fa fa-circle fa-stack-2x"></i>
+                      <i class="fa fa-envelope fa-stack-1x fa-inverse"></i>
+                    </span>
+                </div>
+                <div class="media-body">
+                    <span><a href="https://mail.google.com/mail/u/0/#inbox?compose=GTvVlcRwPVcWtwWPmNmNsZVzWlKtFxcpZfNCZvmQRLGrDkfxgDJNBwWNLBTjWsTxLRPStGHJdFwQg">nyecruz30@gmail.com</a></span>
+                </div>
+            </li>
+              
+            </ul>
+    </div>
+</div>
+</div>
+<!--  INFORMATION : END  -->            	
+            </div>
+        </div>
+    </div>
+
+    
---
+++ includes/powered-slider.php
@@ -0,0 +1,23 @@
+<div id="brands-carousel" class="logo-slider wow fadeInUp">
+		<h3 class="section-title">Powered by:</h3>
+		<div class="logo-slider-inner">	
+			<div id="brand-slider" class="owl-carousel brand-slider custom-carousel owl-theme">
+				<div class="item">
+					<a href="#" class="image">
+						<img data-echo="powered/neust1.png" src="assets/images/blank.gif" alt="">
+					</a>		
+				</div>
+
+				<div class="item">
+					<a href="#" class="image">
+						<img data-echo="powered/neust2.png" src="assets/images/blank.gif" alt="">
+					</a>	
+				</div>
+
+
+
+
+		    </div>
+		</div>
+	
+</div>
